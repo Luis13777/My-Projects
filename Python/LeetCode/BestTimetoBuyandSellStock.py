106a1b440c9bf19590bb90b42218cd1aa5253233
Luis13777 <luis.bertuol.8865@ga.ita.br>
2023-08-01 15:43:31 -0300

    Organizando pastas

---
+++ Python/LeetCode/BestTimetoBuyandSellStock.py
@@ -0,0 +1,22 @@
+from typing import List
+-
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+
+        maxProfit = 0
+
+        while r < len(prices):
+            if prices[l] < prices [r]:
+                profit = prices [r] - prices [l]
+                if (profit > maxProfit):
+                    maxProfit = profit
+            else:
+                l = r
+            r += 1
+
+        return maxProfit 
+    
+
+solucao = Solution()
+print(solucao.maxProfit ([1,2,3]))
